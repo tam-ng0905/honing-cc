d229f2849495ae3a0846e83e9ac3bcc5ea0145f1
Tam Nguyen <ttamng0905@gmail.com>
2021-03-11 23:42:21 -0700

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/honing_cc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/honing_cc.dir/merge-sort.cpp.o"
+  "honing_cc"
+  "honing_cc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/honing_cc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
